25b4c391dcc6fba95021f848381adfae3c152c95
Brookpeng <2411363896@qq.com>
2021-01-13 21:40:28 +0800

    初始化项目

---
+++ qn-ui-1/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Peng />
+</template>
+
+<script>
+import Peng from "./components/Peng.vue";
+
+export default {
+  name: "App",
+  components: {
+    Peng,
+  },
+};
+</script>
